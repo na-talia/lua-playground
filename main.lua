29516aba1a2048b10e1c6c12f76c9a280d258748
na-talia <ntsh_89@ukr.net>
2024-03-13 01:00:20 +0100

    data types and variables

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
 print("Hello")
+
+A = 5 -- global variables
+local a = false -- locale variable
+
+-- Types
+
+local b -- nill
+local c = 1 -- number
+local d = "hello" -- string
+local e = true -- boolean
+-- table (will be added later)
+
+print (b,c,d,e)
+
+
+
